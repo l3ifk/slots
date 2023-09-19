d2aaa77482092c85c7cbb3c038532368526d162b
l3ifk <100339546+l3ifk@users.noreply.github.com>
2023-09-19 08:36:22 +0200

    Add files via upload

--- slots.py
+++ slots.py
@@ -1,5 +1,21 @@
 import pygame
+import random
 
-(width, height) = (400, 300)
-pygame.display.set_mode((width,height))
+pygame.init()
+
+(width, height) = (1300, 800)
+screen = pygame.display.set_mode((width,height))
 pygame.display.flip()
+pygame.display.set_caption("Slots by Kerem & Leon")
+bg = pygame.image.load("background1.jpg")
+pygame.display.toggle_fullscreen()
+screen.blit(bg, (0,0))
+pygame.display.update()
+
+running = True
+while running:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      running = False
+
+
